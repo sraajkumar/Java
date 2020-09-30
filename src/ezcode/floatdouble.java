3802932b7df85e9fa98eebe409611b44d6d58da6
Raajkumar Sundarrajan <raajkumartup@gmail.com>
2020-09-30 17:35:23 +0530

    First Commit

---
+++ src/ezcode/floatdouble.java
@@ -0,0 +1,16 @@
+package ezcode;
+
+public class floatdouble {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+		int myInt = 5/2;
+	System.out.println(myInt);	
+		float myFloat = 5f/3;
+	System.out.println(myFloat);
+		double myDouble =5.0/3;
+	System.out.println(myDouble);
+	}
+
+}
---
+++ src/ezcode/lbstokgs.java
@@ -0,0 +1,13 @@
+package ezcode;
+
+public class lbstokgs {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+double lbs=200;
+double kg = lbs * 0.45359237;
+System.out.println(lbs +" Pounds equals to " + kg + "Kgs");
+
+	}
+
+}
